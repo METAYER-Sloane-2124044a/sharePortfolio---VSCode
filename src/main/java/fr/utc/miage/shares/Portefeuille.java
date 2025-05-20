68fe29c88f0ef5a7ff40127d745687e25c3f5bf5
Remjz <remi.jauzionpro@gmail.com>
2025-05-20 14:49:12 +0200

    CreatePortefeuille

---
+++ src/main/java/fr/utc/miage/shares/Portefeuille.java
@@ -0,0 +1,18 @@
+package fr.utc.miage.shares;
+
+public class Portefeuille {
+
+    private int value;
+
+    public Portefeuille() {
+        this.value = 0;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
