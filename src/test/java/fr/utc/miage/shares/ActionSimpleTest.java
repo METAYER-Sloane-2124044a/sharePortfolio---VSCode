c9122025c74e224d00ce93b7a914fe8ff68aebd0
Sloane Métayer <sloane.metayer@sfr.fr>
2025-05-20 16:05:43 +0200

    fix: Converted action value format to float | Fixed #20:testEnregistrerCoursActionShouldWork

--- src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
+++ src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
@@ -18,8 +18,24 @@ public class ActionSimpleTest {
         action2.enrgCours(jour, 2);
 
         Assertions.assertAll("Set",
-            () -> Assertions.assertDoesNotThrow(() ->  action.enrgCours(jour, 3)), // Premier enregistrement 
-            () -> Assertions.assertDoesNotThrow(() ->  action2.enrgCours(jour, 3))  // Second enregistrement
+            () -> Assertions.assertDoesNotThrow(() ->  action.enrgCours(jour, 3f)), // Premier enregistrement 
+            () -> Assertions.assertDoesNotThrow(() ->  action2.enrgCours(jour2, 3f))  // Second enregistrement
+        );
+    }
+
+    @Test
+    void testEnregistrerCoursActionWithIncorrectParametersShouldThrowException(){
+        final ActionSimple action = new ActionSimple(FOO_SHARE1);
+
+        final Jour jour = new Jour(2025, 1);
+        final Jour jour2 = new Jour(2025, 5);
+
+        action.enrgCours(jour, 2);
+
+        Assertions.assertAll("Set",
+            () -> Assertions.assertThrows(IllegalArgumentException.class, () -> action.enrgCours(jour, 3f)),  // Même jour
+            () -> Assertions.assertThrows(IllegalArgumentException.class, () ->  action.enrgCours(jour2, 0f)),  // <=0
+            () -> Assertions.assertThrows(IllegalArgumentException.class, () ->  action.enrgCours(jour2, -5f))  // <= 0
         );
     }
 }
