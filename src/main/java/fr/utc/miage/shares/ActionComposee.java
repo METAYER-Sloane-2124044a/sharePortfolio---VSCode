df591e200272a14d1975ea2287c9e35062bed8d5
Lucynda <lucynda.carlier@gmail.com>
2025-05-20 15:52:35 +0200

    Création de la classe ActionComposee [Test]: Créer une action composée #22

---
+++ src/main/java/fr/utc/miage/shares/ActionComposee.java
@@ -0,0 +1,91 @@
+package fr.utc.miage.shares;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ActionComposee extends Action{
+
+    private static final int DEFAULT_ACTION_VALUE = 0;
+    private List<ActionSimple> actions = new ArrayList<>();
+    private List<Float> fractions = new ArrayList<>();
+
+    // attribut lien
+    private final Map<Jour, Float> mapCours;
+
+    // constructeur
+    public ActionComposee(final String libelle, List<ActionSimple> aListOfActions, List<Float> aListOfFractions) {
+        // Action simple initialisée comme 1 action
+        super(libelle);
+
+        Float sommeFractions = Float.valueOf(0);
+        
+        if (aListOfActions.size() != aListOfFractions.size()) throw new IllegalArgumentException("Les deux listes doivent être de même longueur.");
+        if (aListOfActions.size() < 2) throw new IllegalArgumentException("La liste d'actions doit avoir une longueur supérieure à 1.");
+        // init spécifique
+        this.mapCours = new HashMap<>();
+        
+        for (ActionSimple anAction : aListOfActions) {
+            this.actions.add(anAction);
+        }
+
+        for (Float aFraction : aListOfFractions) {
+            if (aFraction <= 0 || aFraction >= 1) throw new IllegalArgumentException("La fraction d'action doit être comprise entre 0 et 1 non inclus.");
+            
+            sommeFractions += aFraction;
+            
+            this.fractions.add(aFraction);
+        }
+
+        if (sommeFractions != 1) {
+            throw new IllegalArgumentException("La somme des fractions doit être égale à 1 !");
+        }
+
+        
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((actions == null) ? 0 : actions.hashCode());
+        result = prime * result + ((fractions == null) ? 0 : fractions.hashCode());
+        result = prime * result + ((mapCours == null) ? 0 : mapCours.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ActionComposee other = (ActionComposee) obj;
+        if (actions == null) {
+            if (other.actions != null)
+                return false;
+        } else if (!actions.equals(other.actions))
+            return false;
+        if (fractions == null) {
+            if (other.fractions != null)
+                return false;
+        } else if (!fractions.equals(other.fractions))
+            return false;
+        if (mapCours == null) {
+            if (other.mapCours != null)
+                return false;
+        } else if (!mapCours.equals(other.mapCours))
+            return false;
+        return true;
+    }
+
+    @Override
+    public float valeur(Jour j) {
+        return this.mapCours.getOrDefault(j, Float.valueOf(DEFAULT_ACTION_VALUE));
+    }
+
+    
+}
