f04fe354970301d5585b05b0fe2d894b9bd202b6
Remjz <remi.jauzion81@gmail.com>
2025-05-21 10:00:05 +0200

    [Test]: Retirer un fond sur son compte bancaire supérieur au fond disponible

--- src/main/java/fr/utc/miage/shares/Portefeuille.java
+++ src/main/java/fr/utc/miage/shares/Portefeuille.java
@@ -45,6 +45,8 @@ public class Portefeuille {
     public void retirerDesFonds(double valeur) {
         if (valeur < 0) {
             throw new IllegalArgumentException("Merci de saisir un montant supérieur à 0");
+        } else if (valeur > this.value) {
+            throw new IllegalArgumentException("Merci de saisir un montant inférieur à la valeur du portefeuille");
         }
         this.value = this.value - valeur;
     }
