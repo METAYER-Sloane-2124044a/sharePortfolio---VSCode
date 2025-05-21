3a76d87a711de86100977c8ff1519fdbc071f379
Lucynda <lucynda.carlier@gmail.com>
2025-05-21 09:45:41 +0200

    Ajout de la licence

--- src/main/java/fr/utc/miage/shares/ActionComposee.java
+++ src/main/java/fr/utc/miage/shares/ActionComposee.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 David Navarre &lt;David.Navarre at irit.fr&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fr.utc.miage.shares;
 
 import java.util.ArrayList;
@@ -84,7 +100,7 @@ public class ActionComposee extends Action{
 
     @Override
     public float valeur(Jour j) {
-        return this.mapCours.getOrDefault(j, Float.valueOf(DEFAULT_ACTION_VALUE));
+       return this.mapCours.getOrDefault(j, Float.valueOf(DEFAULT_ACTION_VALUE));
     }
 
     @Override
@@ -92,6 +108,5 @@ public class ActionComposee extends Action{
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'visualiserAction'");
     }
-
     
 }
